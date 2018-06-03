1fcc4668ae4bdda3f6357ecc1a8eed26344ba7b0
Thoai Nguyen <me@thoaionline.com>
2018-06-03 11:11:46 +1000

    Add RHoK plugin

---
+++ craft/plugins/rhok/RhokPlugin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Craft;
+
+class RhokPlugin extends BasePlugin
+{
+    public function getVersion()
+    {
+        return '0.1';
+    }
+
+    public function getDeveloper()
+    {
+        return 'RHoK Melbourne';
+    }
+
+    public function getDeveloperUrl()
+    {
+        return 'http://www.rhokaustralia.org/melbourne/';
+    }
+
+}
