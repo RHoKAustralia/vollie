885d77ff95b0e1fe19c429462659114a4c6d773a
Thoai Nguyen <me@thoaionline.com>
2018-06-03 12:15:32 +1000

    Filter projects by application close date

--- craft/plugins/rhok/consolecommands/ProjectsCommand.php
+++ craft/plugins/rhok/consolecommands/ProjectsCommand.php
@@ -6,11 +6,17 @@ class ProjectsCommand extends BaseCommand
 {
     public function actionSendUpdateRequests()
     {
-        $projects = craft()->rhok_projects->getProjectsByStatuses(['pending', 'active']);
+        $now = new \DateTime();
+        $rawProjects = (array)craft()->rhok_projects->getProjectsByStatuses(['pending', 'active'])->find();
 
-        /** @var EntryModel $project */
-        foreach ($projects as $project) {
-            print_r($project->getContent()->projectStatus);
-        }
+        $applicableProjects = array_filter($rawProjects, function ($project) use ($now) {
+            return ($project->projectStatus == 'active') ||
+                ($project->projectStatus == 'pending' && $project->datesApplicationsClose < $now);
+        });
+
+        array_map(function ($project) {
+            // print_r([(string)$project->projectStatus, $project->datesApplicationsClose->format('r')]);
+        }, $applicableProjects);
+
     }
 }
